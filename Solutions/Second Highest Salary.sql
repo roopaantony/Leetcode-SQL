3e9f1d4034292e619646a2fbe39d57fd95317dc2
roopaantony <roopa3antony@gmail.com>
2024-04-01 16:33:48 +0530

    Update Second Highest Salary.sql

--- Solutions/Second Highest Salary.sql
+++ Solutions/Second Highest Salary.sql
@@ -1,2 +1,10 @@
+--Solution 1--
 SELECT MAX(SALARY) as SecondHighestSalary FROM Employee 
 WHERE SALARY NOT IN (SELECT MAX(SALARY) FROM Employee)
+
+--Solution 2--
+SELECT
+(SELECT DISTINCT SALARY 
+from Employee ORDER BY SALARY DESC 
+LIMIT 1 OFFSET 1) 
+as SecondHighestSalary;
