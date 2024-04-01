3fc72edac1a8b3e501dfab0e263f63c1178ecf6e
roopaantony <roopa3antony@gmail.com>
2024-04-01 17:07:29 +0530

    Update Employees Earning More Than Their Managers.sql

--- Solutions/Employees Earning More Than Their Managers.sql
+++ Solutions/Employees Earning More Than Their Managers.sql
@@ -1,5 +1,5 @@
-SELECT t1.name AS Employee 
+SELECT t2.name AS Employee 
 FROM Employee t1
-JOIN (SELECT id, name, salary from Employee) t2
-on t1.managerID= t2.id
-and t1.SALARY > t2.SALARY
+JOIN Employee t2
+on t1.id= t2.managerID
+and t1.SALARY < t2.SALARY
