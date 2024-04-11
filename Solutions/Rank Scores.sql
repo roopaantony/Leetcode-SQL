af0a5a47ceede0be36f7aa51f414cc55d2daae9e
roopaantony <roopa3antony@gmail.com>
2024-04-11 15:23:27 +0530

    Create Rank Scores.sql

---
+++ Solutions/Rank Scores.sql
@@ -0,0 +1,17 @@
+--MS SQL Server
+--DENSE_RANK() supported in MYSQL 8.0 or lated. Older versions it is not supported
+SELECT score, 
+DENSE_RANK() OVER (ORDER BY score DESC) as rank
+FROM Scores
+
+--MySQL
+SELECT score, CAST(rank_score AS SIGNED) as 'rank'
+FROM
+(
+SELECT score, 
+@rank := CASE WHEN @prev = score THEN @rank ELSE @rank + 1 END as rank_score,
+@prev := score
+FROM Scores,
+(SELECT @rank:=0, @prev=NULL) init
+ORDER BY Score Desc
+) AS A;
