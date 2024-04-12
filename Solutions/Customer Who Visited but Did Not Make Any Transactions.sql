8fa4dd447a11355cb22ff95022dc9d6f6252a8a0
roopaantony <roopa3antony@gmail.com>
2024-04-12 16:16:51 +0530

    Create Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Solutions/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+SELECT customer_id, 
+       COUNT(V.customer_id) AS count_no_trans
+    FROM Visits V
+LEFT JOIN Transactions T
+ON V.visit_id = T.visit_id
+WHERE T.transaction_id IS NULL
+GROUP BY customer_id
